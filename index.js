ffaa86d7335ac0cb111e4825f25187179b12ea2b
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2021-06-11 18:34:04 +0530

    filtered valid artical links and seperated valid links

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"using strict";
 //Importing request Library
 const request = require("request");
 
@@ -9,27 +10,47 @@ const cheerio = require("cheerio");
 request(
   "https://timesofindia.indiatimes.com/",
   function (error, response, html) {
-    // console.log(html);
+    let arraytotallinks = [];
 
-    if (!error && response.statusCode >= 200 /* && response.status < 300 */) {
-      console.log("this 200");
+    if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
 
       $("a").each((i, el) => {
-        const item = $(el).text();
+        // const item = $(el).text();
         const link = $(el).attr("href");
-        let arraylinks = [];
 
-        arraylinks.push(link.trim());
+        arraytotallinks.push(link.trim());
 
         // console.log(link);
-
-        console.log(arraylinks);
       });
     } else {
       console.log(response.status);
-      console.log("this is else");
     }
+
+    // console.log(arraylinks);
+    // console.log(arraylinks.length);
+    // console.log(arraylinks[1]);
+
+    let i;
+    let arrayvalidlinks = [];
+    // const patt = new RegExp('/articlelist/[0-9][".cms"];');
+
+    for (i = 0; i < arraytotallinks.length; i++) {
+      if (arraytotallinks[i].includes("/article")) {
+        // console.log(arraylinks[i]);
+        arrayvalidlinks.push(arraytotallinks[i]);
+      }
+    }
+
+    console.log(`total numbers of links are ${arraytotallinks.length}`);
+    console.log(`total valid numbers links are ${arrayvalidlinks.length}`);
+    console.log(
+      `total unvalid numbers of links are ${
+        arraytotallinks.length - arrayvalidlinks.length
+      }`
+    );
+
+    console.log(arrayvalidlinks);
   }
 );
 
